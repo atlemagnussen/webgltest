9551297ba6c602fc946ce3bd4fda5d1d0cfb9305
Atle Magnussen <atlemagnussen@gmail.com>
2021-11-07 15:11:40 +0100

    handle static

--- src/demos/threeSvgLoader/main.ts
+++ src/demos/threeSvgLoader/main.ts
@@ -1,13 +1,14 @@
 import { renderSVG } from "./svg"
 import { setupScene } from "./scene"
 const extrusion = 1
-
+//@ts-ignore
+import svgUrl from "@app/assets/digilean.svg"
 let update = (extrusion: number) => {}
 
 export const setup = async (canvas: HTMLCanvasElement) => {
     const scene = setupScene(canvas)
 
-    const render = await renderSVG(extrusion, "./assets/digilean.svg")
+    const render = await renderSVG(extrusion, svgUrl)
     // 
     update = render.update
     scene.add(render.object)
