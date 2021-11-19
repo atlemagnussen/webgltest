e9715027401b646c330a48ab5e3da2119bfeedb3
Atle Magnussen <atlemagnussen@gmail.com>
2021-11-19 17:42:46 +0100

    prep babylon and blender

---
+++ src/demos/babylontest/main.ts
@@ -0,0 +1,13 @@
+let anim = 0
+
+export const stop = () => {
+    if (anim)
+        cancelAnimationFrame(anim)
+}
+
+export const resize = (width: number, height: number) => {
+    
+}
+
+let popup: HTMLDivElement
+export const setup = (canv: HTMLCanvasElement, width: number, height: number, pop: HTMLDivElement) => {
